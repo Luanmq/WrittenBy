401147fb8069ff10fbe5df6488b3df89fe2ecb9c
Luanmq <luanmelo.q@gmail.com>
2023-03-20 14:56:11 -0300

    button com startIcon e endIcon

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,17 @@
 import "../Button/Button.css";
+import PropTypes from "prop-types";
 
 export default function Button(props) {
-  return <button class="button-default text-small">{props.text}</button>;
+  return (
+    <button className="button_default text_small">
+      <span className="button_icon">{props.startIcon}</span>
+      {props.text}
+      <span className="button_icon">{props.endIcon}</span>
+    </button>
+  );
 }
+
+//tipo das props
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+};
